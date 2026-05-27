fd8dbb6a2cec0837f3987b84d12bea235a867015
Rathish121005 <aarockyarathishraj2005@gmail.com>
2026-05-27 15:13:43 +0530

    day 3 added

--- eduhub-backend/src/main/java/com/eduhub/eduhub_backend/Controller/CourseController.java
+++ eduhub-backend/src/main/java/com/eduhub/eduhub_backend/Controller/CourseController.java
@@ -2,6 +2,7 @@ package com.eduhub.eduhub_backend.Controller;
 
 
 import com.eduhub.eduhub_backend.Component.Course;
+import com.eduhub.eduhub_backend.Exception.SourceNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @RestController
 public class CourseController {
-    private final Course course;
+
     List<Course>courseList=new ArrayList<>();
 
     public  CourseController(Course course){
@@ -20,7 +21,6 @@ public class CourseController {
         courseList.add(new Course("U23CS408", "OSS", 4));
         courseList.add(new Course("U23IT408", "CC", 4));
         courseList.add(new Course("U23CS408", "OOSE", 4));
-        this.course = course;
     }
 
 
@@ -34,38 +34,48 @@ public class CourseController {
     public ResponseEntity<Course>getCourse(@PathVariable("id") String courseCode)
     {
         for(Course i:courseList) {
-            if (i.getCourseCode().equals(courseCode)) {
+            if(i.getCourseCode().equals(courseCode)) {
+                return ResponseEntity.ok(i);
+            }
+        }
+        return courseList.stream().filter(course -> course.getCourseCode().equalsIgnoreCase(courseCode))
+                .findFirst().map(ResponseEntity::ok)
+                .orElseThrow(()->new SourceNotFound("Course","courseCode",courseCode));
+
+
+    }
+
+    @GetMapping("courses/search")
+    public ResponseEntity<Course>searchCourse(@RequestParam String courseCode)
+    {
+        for(Course i:courseList) {
+            if(i.getCourseCode().equals(courseCode)) {
                 return ResponseEntity.ok(i);
             }
         }
         return ResponseEntity.notFound().build();
-
     }
 
-
-    @PostMapping("add")
+    @PostMapping("courses/add")
     public ResponseEntity<Course>addCourse(@RequestBody Course course)
     {
         courseList.add(course);
         return ResponseEntity.ok(course);
     }
 
-    @PutMapping("updateCourse/{id}")
+    @PutMapping("courses/update/{id}")
     public ResponseEntity<Course>updateCourse(@PathVariable("id") String courseCode,@RequestBody Course course)
     {
-        for(Course i:courseList)
-        {
-            if(i.getCourseCode().equals(courseCode))
-            {
-                i.setCredits(course.getCredits());
-                i.setCourseName(course.getCourseName());
-                return ResponseEntity.ok(i);
-            }
-        }
-        return ResponseEntity.notFound().build();
+        Course course1=courseList.stream().filter(course2->course2.getCourseCode().equalsIgnoreCase(courseCode))
+                .findFirst()
+                .orElseThrow(()->new SourceNotFound("Course","CourseCode",courseCode));
+        course1.setCourseName(course.getCourseName());
+        course1.setCredits(course.getCredits());
+
+        return ResponseEntity.ok(course1);
     }
 
-    @DeleteMapping("deleteCourse/{id}")
+    @DeleteMapping("courses/delete/{id}")
     public  ResponseEntity<List<Course>>deleteCourse(@PathVariable("id") String courseCode)
     {
         for(Course i:courseList)
@@ -77,5 +87,17 @@ public class CourseController {
         }
         return ResponseEntity.ok(courseList);
     }
+
+    @PutMapping("/query/{id}")
+    public String queryCourse(@PathVariable("id") String courseCode)
+    {
+        if(courseCode.startsWith("*"))
+        {
+            throw new SourceNotFound("Course","It having special character",courseCode);
+        }
+
+        return courseCode;
+    }
+
 
 }
